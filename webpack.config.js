e6d4703e4b08e79dcd67d64680ed117b3d92637f
Sergey Lukin <contact@sergeylukin.com>
2015-11-19 06:24:37 +0200

    Install webpack with essential development goodies
    - Webpack - build orchestrator
    
      ```
      npm i webpack --save-dev
      ```
    
    - Generate index.html automatically
    
      ```
      npm i html-webpack-plugin --save-dev
      ```
    
    - Dev server with Hot loading
    
      ```
      npm i webpack-dev-server --save-dev
      ```
    
      Additionally, we set a shortcut to package.json for `webpack-dev-server`
      so we can run it by simply executing: `npm start`

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  entry: APP_PATH,
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Netwalk game'
+    })
+  ]
+};
