76ff93542e157dfae25b1b81cd118f71895327cf
Sergey Lukin <contact@sergeylukin.com>
2015-11-26 23:52:46 +0200

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+webpack && \
+cd ./build && \
+remote_repo=`git config remote.origin.url` && \                                                    
+remote_branch="gh-pages" && \
+git init && \
+git add . && \
+git commit -m'build' && \
+git push $remote_repo master:$remote_branch --force && \
+rm -fr .git && \
+cd ../
