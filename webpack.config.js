187ceae21bd21dfe6c2647b1a4034dec7dd3ee85
Sergey Lukin <contact@sergeylukin.com>
2015-11-28 04:33:34 +0200

    Enhance webpack tooling
    - Use `npm run deploy` to push project to Github Pages:
    
      ```
      npm i gh-pages --save-dev
      ```
    
    - Use `npm run build` to build compressed project with
      vendor JS, app JS and CSS in separate splits:
    
      ```
      npm i clean-webpack-plugin --save-dev
      npm i extract-text-webpack-plugin --save-dev
      ```

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,35 @@ var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var merge = require('webpack-merge');
+var Clean = require('clean-webpack-plugin');
 
-var TARGET = process.env.npm_lifecycle_event;
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(ROOT_PATH, 'app');
-var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var pkg = require('./package.json');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
 
 process.env.BABEL_ENV = TARGET;
 
 var common = {
-  entry: APP_PATH,
+  entry: PATHS.app,
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   output: {
-    path: BUILD_PATH,
+    path: PATHS.build,
     filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: APP_PATH
-      },
-      {
         test: /\.jsx?$/,
         loaders: ['babel'],
-        include: APP_PATH
+        include: PATHS.app
       }
     ]
   },
@@ -42,6 +43,61 @@ var common = {
 
 if (TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
-    devtool: 'eval-source-map'
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: PATHS.app
+        }
+      ]
+    },
+    devtool: 'eval-source-map',
+    plugins: [
+      new webpack.DefinePlugin({
+        __DEV__: true
+      })
+    ]
+  });
+}
+
+if(TARGET === 'build' || TARGET === 'stats' || TARGET === 'deploy') {
+  module.exports = merge(common, {
+    entry: {
+      app: PATHS.app,
+      vendor: Object.keys(pkg.dependencies)
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js?'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.app
+        }
+      ]
+    },
+    devtool: 'source-map',
+    plugins: [
+      new webpack.DefinePlugin({
+        // This affects react lib size
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        __DEV__: false
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }),
+      new webpack.optimize.CommonsChunkPlugin(
+        'vendor',
+        '[name].[chunkhash].js'
+      ),
+      new Clean(['build']),
+      new ExtractTextPlugin('styles.[chunkhash].css')
+    ]
   });
 }
