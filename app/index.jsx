ab17839a74efce43e51f414768502863625fa07f
Sergey Lukin <contact@sergeylukin.com>
2015-12-16 13:49:51 +0200

    Adjust game board dimensions with respect to viewport dimensions

--- app/index.jsx
+++ app/index.jsx
@@ -5,5 +5,16 @@ import ReactDOM from 'react-dom';
 import NetwalkGame from './components/NetwalkGame';
 
 const app = document.createElement('div');
+app.style.width = '100%';
+app.style.height = document.documentElement.clientHeight+'px';
 document.body.appendChild(app);
-ReactDOM.render(<NetwalkGame rows="5" columns="10" />, app);
+
+const width = app.offsetWidth,
+      height = app.offsetHeight,
+
+      node_size = Math.max(width, height) / 100 * 13,
+
+      rows = Math.floor(height / node_size),
+      columns = Math.floor(width / node_size);
+
+ReactDOM.render(<NetwalkGame rows={rows} columns={columns} />, app);
