5f0044bbd060d7180fc518cfda5a2591b88df766
Sergey Lukin <contact@sergeylukin.com>
2015-12-16 18:07:33 +0200

    Round down the number of nodes

--- app/index.jsx
+++ app/index.jsx
@@ -9,12 +9,32 @@ app.style.width = '100%';
 app.style.height = document.documentElement.clientHeight+'px';
 document.body.appendChild(app);
 
-const width = app.offsetWidth,
-      height = app.offsetHeight,
+let width = app.offsetWidth,
+    height = app.offsetHeight,
+    multiplier = 100;
 
-      node_size = Math.max(width, height) / 100 * 13,
+// Convert 639 to 600, 231 to 200, 
+if (width >= multiplier) {
+  width -= width % multiplier;
+} else {
+  width = multiplier
+}
+if (height >= multiplier) {
+  height -= height % multiplier;
+} else {
+  height = multiplier;
+}
 
-      rows = Math.floor(height / node_size),
-      columns = Math.floor(width / node_size);
+let difficulty_level = 1,
+    node_size;
+
+if (width >= height) {
+  node_size = width / (width / multiplier) / difficulty_level;
+} else if (height > width) {
+  node_size = height / (height / multiplier) / difficulty_level;
+}
+
+const rows = Math.floor(height / node_size);
+const columns = Math.floor(width / node_size);
 
 ReactDOM.render(<NetwalkGame rows={rows} columns={columns} />, app);
